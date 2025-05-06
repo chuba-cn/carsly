35668503e867eece02dcf96086ab2655e2840cc5
chuba <chinemelumchubanwene57@gmail.com>
2025-05-06 16:43:16 +0100

    fix build errors

--- src/components/shared/custom-pagination.tsx
+++ src/components/shared/custom-pagination.tsx
@@ -170,7 +170,6 @@ function CustomPagination(props: CustomPaginationProps) {
               }}
             />
           </PaginationItem>
-          </PaginationItem>
         </PaginationContent>
       </PaginationRoot>
     </div>
